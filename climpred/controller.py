419fcd9389dbf8b508015704b08282dff5a3eb8c
Adrian Mag <zcfbagx@ucl.ac.uk>
2022-10-24 18:29:08 +0300

    #3 cleaned again

--- climpred/controller.py
+++ climpred/controller.py
@@ -1,18 +1,19 @@
 from model import Model
 from view import View
 
+
 class Controller():
     """
     Main controller
     """
-    
+
     def __init__(self):
         self.model = Model()
         self.view = View(self)
-        
+
     def main(self):
         self.view.main()
-        
+
 if __name__ == '__main__':
     calculator = Controller()
     calculator.main()
