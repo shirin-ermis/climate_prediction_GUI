b1dc23b04ec050b02efc00cef6343630fb1bfd96
shirin-ermis <shirin.ermis@stx.ox.ac.uk>
2022-10-25 11:43:01 +0100

    create plot function

--- climpred/plot.py
+++ climpred/plot.py
@@ -1,4 +1,6 @@
-import climpred as cp
+# import climpred as cp
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Plot():
@@ -8,4 +10,19 @@ class Plot():
     """
 
     def __init__(self):
-        
+        self.plot = None
+
+    def creat_plot(self, TS, T1, T2):
+        # Data
+        T = np.array([TS, T1, T2])
+        heights = np.array([0, 1, 2])
+
+        # Plotting
+        fig = plt.figure()
+        plt.plot(T, heights)
+
+        # Plot parameters
+        plt.set_yticklabels(['TS', 'T1', 'T2'])
+        fig.xlim(-100, 100)
+        fig.ylabel('Height')
+        fig.xlabel('Temperature / °C')
