174977fcc5649c089ebc3e11f07420d62a86d1ff
Sam Scivier <samscivier@gmail.com>
2022-10-25 14:26:00 +0100

    #8 fix flake8

--- climpred/albedo.py
+++ climpred/albedo.py
@@ -1,10 +1,10 @@
 def get_albedo(cloud_cover):
     """Function calculates albedo of the planet
     using cloud cover
-    
+
     Assumptions:
     - open ocean underneath clouds only
-    - cloud albedo of 75% (marine stratocumulus, 
+    - cloud albedo of 75% (marine stratocumulus,
         source: https://en.wikipedia.org/wiki/Cloud_albedo)
     - open ocean albedo 6% (source: https://en.wikipedia.org/wiki/Albedo)"""
 
