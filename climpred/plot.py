e267e1d78318e30d748650aae0b471e7041826ee
shirin-ermis <shirin.ermis@stx.ox.ac.uk>
2022-10-25 10:26:26 +0100

    start plot class

---
+++ climpred/plot.py
@@ -0,0 +1,11 @@
+import climpred as cp
+
+
+class Plot():
+    """
+    Class to create plots for the model that displays the temperatures
+    in the atmospheric layers
+    """
+
+    def __init__(self):
+        
