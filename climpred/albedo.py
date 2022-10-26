5f4e73c1df4ab88ca66c077811288ae7f3491e2f
Sam Scivier <samscivier@gmail.com>
2022-10-26 16:31:42 +0100

    #27 add to albedo.py docstring

--- climpred/albedo.py
+++ climpred/albedo.py
@@ -6,7 +6,20 @@ def get_albedo(cloud_cover):
     - open ocean underneath clouds only
     - cloud albedo of 75% (marine stratocumulus,
         source: https://en.wikipedia.org/wiki/Cloud_albedo)
-    - open ocean albedo 6% (source: https://en.wikipedia.org/wiki/Albedo)"""
+    - open ocean albedo 6% (source: https://en.wikipedia.org/wiki/Albedo)
+
+    Parameters
+    ==========
+
+    cloud_cover: float
+        fraction of globe covered by clouds
+
+    Returns
+    =======
+
+    alpha: float
+        planetary albedo
+    """
 
     alpha = cloud_cover * 0.75 + (1 - cloud_cover) * 0.06
     return alpha
