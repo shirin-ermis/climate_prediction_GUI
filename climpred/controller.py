03e6c4c1b37e33870b7260c739eab2b9d5ff0e73
synapsidfan <spencer.pevsner@env-res.ox.ac.uk>
2022-10-27 14:37:19 +0100

    Eliminated print statements from controller.py

--- climpred/controller.py
+++ climpred/controller.py
@@ -15,12 +15,6 @@ class Controller():
 
     def _on_press_calculate_button(self):
         # The input_value will be taken from the sliders
-        cloud_cover = self.view.cloud_cover_slider.get()
-        eps_1 = self.view.epsilon_1_slider.get()
-        eps_2 = self.view.epsilon_2_slider.get()
-        H_S = self.view.H_S_slider.get()
-        H_L = self.view.H_L_slider.get()
-        S_0 = self.view.S_0_slider.get()
         # The overwritten one is just a dummy because now we don't take
         # inputs from sliders yet
         input_value_overwritten = cp.calculate_temperature_matrix(
@@ -31,9 +25,7 @@ class Controller():
             H_L=self.view.H_L_slider.get(),
             S_0=self.view.S_0_slider.get()
         )
-        print(input_value_overwritten)
         my_plot = cp.Plot(input_value_overwritten)
-        print(cloud_cover, eps_1, eps_2, H_S, H_L, S_0)
         self.view._make_graph(my_plot)
 
 
