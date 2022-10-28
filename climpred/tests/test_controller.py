6d3ea843ba068bf65711c89a727b220f10399ab6
Justin Leung <justinleung074170@gmail.com>
2022-10-28 14:36:56 +0100

    updated advanced calculate button test

--- climpred/tests/test_controller.py
+++ climpred/tests/test_controller.py
@@ -1,15 +1,17 @@
 #
 # test_controller.py
 #
-from ctypes.wintypes import HACCEL
 import unittest
+
+from pyparsing import Empty
 import climpred as cp
+
 
 class ControllerTest(unittest.TestCase):
     """
     Tests the :class:`Controller` class.
     """
-    global ctrl 
+    global ctrl
     ctrl = cp.Controller()
 
     def test_init(self):
@@ -21,18 +23,16 @@ class ControllerTest(unittest.TestCase):
         ctrl.view.btn_calc.invoke()
 
     def test_on_press_advanced_calculate_button(self):
+        # setting inputs for running ctrl._on_press_advanced_calculate_button()
         ctrl.view.advanced_btn.invoke()
+
         ctrl.view.layers.set(5)
         ctrl.view.advanced_cloud_cover_slider.set(0.5)
-        # ctrl.view.epsilon = '0.7 0.7 0.7 0.7 0.7'
-        # ctrl.view.H = '5 5 5 5 5'
-        ctrl.view.advanced_epsilon_entry.insert(0, '0.7 0.7 0.7 0.7 0.7'),
-        ctrl.view.advanced_H_entry.insert(0, '5 5 5 5 5'),
+        ctrl.view.advanced_epsilon_entry.insert(0, '0.7 0.7 0.7 0.7 0.7')
+        ctrl.view.advanced_H_entry.insert(0, '5 5 5 5 5')
         ctrl.view.advanced_solar_constant_slider.set(1000)
-        ctrl._on_press_advanced_calculate_button()
-        a = ctrl.view.advanced_calculate_btn.invoke()
 
-        print(a)
+        ctrl._on_press_advanced_calculate_button()
 
 
 if __name__ == "__main__":
