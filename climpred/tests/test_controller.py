cc25ad7c8c701dffab72e3e3f3d844bead939274
Justin Leung <justinleung074170@gmail.com>
2022-10-28 14:04:15 +0100

    Added inputs in tests to avoid errors

--- climpred/tests/test_controller.py
+++ climpred/tests/test_controller.py
@@ -18,12 +18,22 @@ class ControllerTest(unittest.TestCase):
 
     def test_on_press_calculate_button(self):
         ctrl._on_press_calculate_button()
-        ctrl.view.btn.invoke()
+        ctrl.view.btn_calc.invoke()
 
     def test_on_press_advanced_calculate_button(self):
+        ctrl.view.advanced_btn.invoke()
+        ctrl.view.layers.set(5)
+        ctrl.view.advanced_cloud_cover_slider.set(0.5)
+        # ctrl.view.epsilon = '0.7 0.7 0.7 0.7 0.7'
+        # ctrl.view.H = '5 5 5 5 5'
+        ctrl.view.advanced_epsilon_entry.insert(0, '0.7 0.7 0.7 0.7 0.7'),
+        ctrl.view.advanced_H_entry.insert(0, '5 5 5 5 5'),
+        ctrl.view.advanced_solar_constant_slider.set(1000)
         ctrl._on_press_advanced_calculate_button()
-        ctrl.view.advanced_calculate_btn.invoke()
+        a = ctrl.view.advanced_calculate_btn.invoke()
 
-       
+        print(a)
+
+
 if __name__ == "__main__":
     unittest.main()
